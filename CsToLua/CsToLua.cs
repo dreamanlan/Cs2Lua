956f1edcc1a0b174ccd798cb6194163e367a75ab
dreamanlan <dreaman_163@163.com>
2017-05-31 14:32:11 +0800

    代码里整数操作符列表多了++与--，这2个操作在翻译时已经转换为对应的+与-了。

--- CsToLua/CsToLua.cs
+++ CsToLua/CsToLua.cs
@@ -979,7 +979,7 @@ namespace RoslynTool.CsToLua
         private static HashSet<string> s_UnsupportedUnaryOperators = new HashSet<string> { "&", "*" };
         private static HashSet<string> s_UnsupportedBinaryOperators = new HashSet<string> { "->" };
         //下面这个list的顺序要与utility.lua里的整数操作表__cs2lua_special_integer_operators一致（索引用作操作符识别常量）
-        private static List<string> s_SpecialIntegerOperators = new List<string> { "/", "%", "+", "-", "*", "++", "--", "<<", ">>", "&", "|", "^", "~" };
+        private static List<string> s_SpecialIntegerOperators = new List<string> { "/", "%", "+", "-", "*", "<<", ">>", "&", "|", "^", "~" };
 
         private static Dictionary<string, string> s_UnaryAlias = new Dictionary<string, string> { 
             {"!", "not"}
